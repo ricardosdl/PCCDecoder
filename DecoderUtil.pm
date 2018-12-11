a80b59f8a936cf3ac2e053c2fba18b98306beb08
Ricardo Soares de Lima <ricardo.soaresdelima@gmail.com>
2018-12-11 21:51:05 -0200

    Now it really returns a list of unique chars

--- DecoderUtil.pm
+++ DecoderUtil.pm
@@ -2,17 +2,19 @@ package DecoderUtil;
 use 5.010;
 use strict;
 use warnings;
+use List::MoreUtils qw(uniq);
 
 sub list_unique_chars {
     my ($original_text, $ignored) = @_;
-    #$ignored ||= /[^\d\w]/p;
-    #$ignored ||= qr/[^\d\w]/p;
     
-    say $ignored;
+    $ignored ||= qr/[^\d\w]/p;#will match everything that is not a letter or number
     
     my $new_text = $original_text =~ s/$ignored//rg;
     
-    return $new_text;
+    my @all_chars = split(//, $new_text);
+    my @unique_chars = uniq(@all_chars);
+    
+    return @unique_chars;
     
 }
 
