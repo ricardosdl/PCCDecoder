2e171dfe86886e11dba422d4e7b013596336b4eb
Ricardo Soares de Lima <ricardo-sl@segplan.go.gov.br>
2018-12-13 16:28:31 -0300

    Changed it so it gets and displays the frequency of each letter

--- decoder.pl
+++ decoder.pl
@@ -3,16 +3,26 @@ use strict;
 use warnings;
 use Path::Tiny qw(path);
 use DecoderUtil;
+use utf8;
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
 
 my $targetfilename = shift or die "Usage: $0 FILENAME";
 
 my $targetcontent = path($targetfilename)->slurp_utf8;
 
-my @targetcontent_only_letters = DecoderUtil::list_unique_chars($targetcontent);
-my $num_letters = @targetcontent_only_letters;
+my %letters_frequency = DecoderUtil::letters_frequency($targetcontent);
 
-foreach(@targetcontent_only_letters) {
-    say $_;
+foreach my $l (sort {$letters_frequency{$a} <=> $letters_frequency{$b}} keys %letters_frequency) {
+    printf "%s %s\n", $l, $letters_frequency{$l};
 }
 
-say "There are ", $num_letters;
+#my @targetcontent_only_letters = DecoderUtil::list_unique_chars($targetcontent);
+#my $num_letters = @targetcontent_only_letters;
+#
+#foreach(@targetcontent_only_letters) {
+#    say $_;
+#}
+#
+#say "There are ", $num_letters;
