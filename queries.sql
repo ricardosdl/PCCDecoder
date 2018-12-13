b67e1bc52b01e955e9236dd8a4d15a8f9c206ca2
Ricardo Soares de Lima <ricardo-sl@segplan.go.gov.br>
2018-12-13 16:26:49 -0300

    Some more queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,29 @@
+select A.word, B.word
+from
+(
+    select word from words where length(word) = 4
+) A
+join (select word from words where length(word) = 10) B ON
+    (substr(B.word, 5, 4) = A.word) AND (substr(B.word, 3, 2) = substr(A.word, 3, 2))
+order by A.word, B.word;
+
 select word
 from
 (
     select word from words where length(word) = 18 and substr(word, 1, 2) = substr(word, 17, 2)
 )
 where substr(word, 1, 2) in (select substr(word, 1, 2) from words where length(word) = 8);
+
+select A.word, B.word
+from
+(
+    select word from words where length(word) = 18 and substr(word, 1, 2) = substr(word, 17, 2)
+) A
+join (select word from words where length(word) = 8) B
+    ON (substr(B.word, 1, 2) = substr(A.word, 1, 2)) AND
+    (substr(B.word, 5, 2) = substr(A.word, 12, 2));
+
+
 
 select word from (select word from words where length(word)=10);
 
